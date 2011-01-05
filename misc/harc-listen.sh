1ff25cb135aab14a0810fc98515d05df55fa16db
Bengt Martensson <bengtmartensson@users.noreply.github.com>
2011-01-05 19:09:51 +0000

    misc updates to new setting

--- misc/harc-listen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-HARC_HOME=/home/bengt/harc/harc
-export HARC_HOME
-READLINE_LIB=/usr/local/lib
-
-java -Djava.library.path=${READLINE_LIB} -classpath ${HARC_HOME}/harc.jar harc/irtrans -v ${HARC_HOME}/config/listen.xml 
